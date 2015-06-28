5cb5ae127db99fc0851b09586d7b893c7dc703aa
oranges <email@oranges.net.nz>
2015-06-28 20:56:31 +1200

    Modify main.h to support linux compilation
    This was based on best pratice from here
    https://gcc.gnu.org/wiki/Visibility

--- main.h
+++ main.h
@@ -26,11 +26,31 @@
 extern "C"{
 #endif // __cplusplus
 
-#if defined(BUILD_DLL)
-	#define EXPORT __declspec(dllexport)
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef BUILDING_DLL
+    #ifdef __GNUC__
+      #define EXPORT __attribute__ ((dllexport))
+    #else
+      #define EXPORT __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+    #endif
+  #else
+    #ifdef __GNUC__
+      #define EXPORT __attribute__ ((dllimport))
+    #else
+      #define EXPORT __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
+    #endif
+  #endif
+  #define DLL_LOCAL
 #else
-    #define EXPORT __declspec(dllimport)
+  #if __GNUC__ >= 4
+    #define EXPORT __attribute__ ((visibility ("default")))
+    #define DLL_LOCAL  __attribute__ ((visibility ("hidden")))
+  #else
+    #define EXPORT
+    #define DLL_LOCAL
+  #endif
 #endif
+
 
 char EXPORT *regEx_compare(int argc, char *argv[]);
 char EXPORT *regex_compare(int argc, char *argv[]);
